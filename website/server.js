d9650bea6141a424459c82eae0df97f558e73298
jovanpet <pjovan65@gmail.com>
2022-01-23 10:49:39 -0600

    website 1.5

--- website/server.js
+++ website/server.js
@@ -1,5 +1,3 @@
-
-
 // include the express modules
 var express = require("express");
 
@@ -35,49 +33,53 @@ app.listen(9007, () => console.log('Listening on port 9007!'));
 
 // function to return the index page
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/client/index.html');
+    res.sendFile(__dirname + '/client/index.html');
 })
 
 // function to return the index page
 app.get('/index', function (req, res) {
-  res.sendFile(__dirname + '/client/index.html');
+    res.sendFile(__dirname + '/client/index.html');
 });
 
 // function to return the gallery page
 app.get('/gallery', function (req, res) {
-  res.sendFile(__dirname + '/client/gallery.html');
+    res.sendFile(__dirname + '/client/gallery.html');
 });
 
 // function to return the add Image page
 app.get('/addImage', function (req, res) {
-  res.sendFile(__dirname + '/client/addImage.html');
+    res.sendFile(__dirname + '/client/addImage.html');
 });
 
 // function to return the add event page
 app.post('/send_password', function (req, res) {
 
-  let password = req.body.password;
-  const python = spawn('python3', ['script.py', password]);
+    fs.readdir(dire, (err, files) => {
+        counter = files.length;
+        let password = req.body.password;
+        const python = spawn('python3', ['script.py', password, (counter + 1)]);
+    })
 
-  //DELETE THIS AFTER IT JST TRACKS BUFFER FROM PYTHONs
-  python.stdout.on('data', function (data) {
-    console.log('Pipe data from python script ...');
-    dataToSend = data.toString();
-    console.log(dataToSend);
-  });
 
-  res.redirect("/gallery");
+    //DELETE THIS AFTER IT JST TRACKS BUFFER FROM PYTHONs
+    python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+        console.log(dataToSend);
+    });
+
+    res.redirect("/gallery");
 });
 
 app.get('/getImages', function (req, res) {
 
-  counter=0;
-  const dire = './images';
-  fs.readdir(dire, (err, files) => {
-    counter=files.length;
-    res.send({num:counter});
-  })
-  
+    counter = 0;
+    const dire = './images';
+    fs.readdir(dire, (err, files) => {
+        counter = files.length;
+        res.send({ num: counter });
+    })
+
 });
 
 // middle ware to serve static files
@@ -85,6 +87,6 @@ app.use('/client', express.static(__dirname + '/client'));
 
 // function to return the 404 message and error to client
 app.get('*', function (req, res) {
-  // add details
-  res.sendStatus(404);
+    // add details
+    res.sendStatus(404);
 });
