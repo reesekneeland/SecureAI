d8a0b9c554eb2eab08c027254cd997ea76b8345a
reesekneeland <reesekrocks@gmail.com>
2022-01-23 10:29:49 -0600

    adding python scripts

--- README.md
+++
@@ -1,2 +0,0 @@
-# minnehack22
-A code repository for projects created during Minnehack 2022
--- pass_checkerf.py
+++
@@ -1,6 +0,0 @@
-
-def passStrength(password, Stats):
-    num = round((Stats(password).strength()*100))
-    if num != 0: dec = round((Stats(password).strength()*100) % num, 3)
-    else: dec = round((Stats(password).strength()*100), 3)
-    return 100 if num == 99 and dec > 0.99 else 0 if num == 0 and dec < 0.01 else num
